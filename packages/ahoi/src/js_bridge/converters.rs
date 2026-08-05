72c737beab421ddd8c23824a612d1b8d9b68043e
Acheul <seoul605.21@gmail.com>
2026-08-05 11:44:33 +0900

    add serde_json converter

--- packages/ahoi/src/js_bridge/converters.rs
+++ packages/ahoi/src/js_bridge/converters.rs
@@ -3,16 +3,18 @@ use serde::{Serialize, de::DeserializeOwned};
 
 // "serde-wasm-bindgen"
 
+#[cfg(feature = "serde-wasm-bindgen")]
 pub use serde_wasm_bindgen_converter::SerdeWasmBindgenConverter;
 
+#[cfg(feature = "serde-wasm-bindgen")]
 mod serde_wasm_bindgen_converter {
     use super::*;
     pub struct SerdeWasmBindgenConverter;
 
     impl<T: Serialize + DeserializeOwned> HailConverter<T> for SerdeWasmBindgenConverter {
-        type HailValue = JsValue;
+        type HailValue = wasm_bindgen::JsValue;
 
-        const NONE: Self::HailValue = JsValue::undefined();
+        const NONE: Self::HailValue = wasm_bindgen::JsValue::undefined();
 
         fn from_raw_value(raw_value: &T) -> Self::HailValue {
             serde_wasm_bindgen::to_value(raw_value).unwrap()
@@ -20,6 +22,31 @@ mod serde_wasm_bindgen_converter {
 
         fn into_raw_value(hail_value: Self::HailValue) -> T {
             serde_wasm_bindgen::from_value(hail_value).unwrap()
+        }
+    }
+}
+
+// "serde_json"
+
+#[cfg(feature = "serde_json")]
+pub use serde_json_converter::SerdeJsonConverter;
+
+#[cfg(feature = "serde_json")]
+mod serde_json_converter {
+    use super::*;
+    pub struct SerdeJsonConverter;
+
+    impl<T: Serialize + DeserializeOwned> HailConverter<T> for SerdeJsonConverter {
+        type HailValue = serde_json::Value;
+
+        const NONE: Self::HailValue = serde_json::Value::Null;
+
+        fn from_raw_value(raw_value: &T) -> Self::HailValue {
+            serde_json::to_value(raw_value).unwrap()
+        }
+
+        fn into_raw_value(hail_value: Self::HailValue) -> T {
+            serde_json::from_value(hail_value).unwrap()
         }
     }
 }
